5b6530ea6c4685886aea3a0eb4fc79bd925a21a9
Schkullie <71150548+Schkullie@users.noreply.github.com>
2020-09-20 17:18:22 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+
+var channel = "Instafluff" ;
+//your Twitchname. Leave the "" alone. It will not work without them.
+
+var bot = "BotName";
+//put your custom bot name here
+
+var indicator = 'left';
+//set the little triangle at the bottom to the side you want wwith either 'left' or 'rigth'
+
+var namecolor = 'darkgray';
+//choose the default name color if Twitch doesn't provide one
+
+var badges = 'yes' ;
+//toggle if badges (Mod, VIP, Sub) should be shown. Put 'yes' to activate and 'no' to deactivate
+
+var timestamp = 'yes' ;
+//toggle if a timestamp should be shown. Put 'yes' to activate and 'no' to deactivate
+
+var messages = 3 ;
+//number of messages to be shown at one time
+
+var seconds = 15 ;
+//the time the chat overlay will stay visible, when people stop sending chat messages
